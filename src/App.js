02f1a282491307b76b8a8cf9d7f4ee1e8c4d37e5
Xarri Jorge <kodinglife@gmail.com>
2020-04-07 20:33:25 +0000

    Added gitignore / form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import "./styles.css";
+import React from 'react'
+
+import Form from './components/form'
+import './styles.css'
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Form />
     </div>
-  );
+  )
 }
---
+++ src/components/form.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Form = () => (
+  <form>
+    <input type="text" className="cardHolderName" />
+    <div className="dateBox">
+      <p>
+        <label htmlFor="cardNumber">Card Number</label>
+        <input
+          type="text"
+          className="cardNumber"
+          placeholder="#### #### #### ####"
+        />
+      </p>
+      <input type="text" className="month" />
+      <input type="text" className="year" />
+      <input type="text" className="cvv" />
+    </div>
+  </form>
+)
+
+export default Form
